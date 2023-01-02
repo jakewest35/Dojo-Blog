d78a5fce730efcada9d80fb80179b4e5c3d9e31a
Jake West <jswest95@yahoo.com>
2023-01-01 21:56:44 -0800

    Finished lesson 21

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import NavBar from "./NavBar";
 import Home from "./Home";
 
+import {
+  BrowserRouter,
+  Routes,
+  Route as RouteComponent,
+} from "react-router-dom";
+
 function App() {
   return (
-    <div className="App">
-      <NavBar />
-      <div className="content">
-        <Home />
+    <BrowserRouter>
+      <div className="App">
+        <NavBar />
+        <div className="content">
+          <Routes>
+            <RouteComponent path="/" element={<Home />} />
+          </Routes>
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
